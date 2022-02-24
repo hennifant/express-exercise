2318d156295883ba8e627364abfa4d54f592e9c4
Henning Schwichow <henning.schwichow@gmx.de>
2022-02-24 09:07:13 +0100

    First Commit

--- routes/jokes.js
+++ routes/jokes.js
@@ -5,19 +5,19 @@ const router = express.Router();
 
 let jokes = [
   {
-    id: 0,
+    id: "0",
     joke: "Thanks for explaining the word “many” to me, it means a lot.",
   },
   {
-    id: 1,
+    id: "1",
     joke: "Why did Adele cross the road? To say hello from the other side.",
   },
   {
-    id: 2,
+    id: "2",
     joke: "What kind of concert only costs 45 cents? A 50 Cent concert featuring Nickelback.",
   },
   {
-    id: 3,
+    id: "3",
     joke: "To the person who invented zero, thanks for nothing.",
   },
 ];
@@ -27,7 +27,7 @@ let jokes = [
  * Create a GET /joke route, that returns all jokes.
  */
 router.get("/", (req, res, next) => {
-  // …
+  res.json(jokes);
 });
 
 /**
@@ -35,14 +35,29 @@ router.get("/", (req, res, next) => {
  * Create a GET /joke/:id route, that returns the joke for the given id.
  */
 
-// …
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const joke = jokes.find((joke) => joke.id === id);
+  if (joke) {
+    res.json(joke);
+  } else {
+    next();
+  }
+
+  res.send();
+});
 
 /**
  * Exercise 3
  * Create a POST /joke route, that adds a new joke to the array.
  */
 
-// …
+router.get("/", (req, res, next) => {
+  const joke = req.body;
+  newJoke.id = nanoid();
+  jokes.push(newJoke);
+  res.json(newJoke);
+});
 
 /**
  * Exercise 4
