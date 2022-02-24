8a8bbebac7cf5ca48ccfe05114acd9b21c0aacb2
Henning Schwichow <henning.schwichow@gmx.de>
2022-02-24 09:10:38 +0100

    Second Commit

--- routes/jokes.js
+++ routes/jokes.js
@@ -1,24 +1,24 @@
-import express from "express";
-import { nanoid } from "nanoid";
+import express from 'express';
+import { nanoid } from 'nanoid';
 
 const router = express.Router();
 
 let jokes = [
   {
-    id: "0",
-    joke: "Thanks for explaining the word “many” to me, it means a lot.",
+    id: '0',
+    joke: 'Thanks for explaining the word “many” to me, it means a lot.',
   },
   {
-    id: "1",
-    joke: "Why did Adele cross the road? To say hello from the other side.",
+    id: '1',
+    joke: 'Why did Adele cross the road? To say hello from the other side.',
   },
   {
-    id: "2",
-    joke: "What kind of concert only costs 45 cents? A 50 Cent concert featuring Nickelback.",
+    id: '2',
+    joke: 'What kind of concert only costs 45 cents? A 50 Cent concert featuring Nickelback.',
   },
   {
-    id: "3",
-    joke: "To the person who invented zero, thanks for nothing.",
+    id: '3',
+    joke: 'To the person who invented zero, thanks for nothing.',
   },
 ];
 
@@ -26,7 +26,7 @@ let jokes = [
  * Exercise 1
  * Create a GET /joke route, that returns all jokes.
  */
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.json(jokes);
 });
 
@@ -35,9 +35,9 @@ router.get("/", (req, res, next) => {
  * Create a GET /joke/:id route, that returns the joke for the given id.
  */
 
-router.get("/:id", (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   const { id } = req.params;
-  const joke = jokes.find((joke) => joke.id === id);
+  const joke = jokes.find(joke => joke.id === id);
   if (joke) {
     res.json(joke);
   } else {
@@ -52,11 +52,13 @@ router.get("/:id", (req, res, next) => {
  * Create a POST /joke route, that adds a new joke to the array.
  */
 
-router.get("/", (req, res, next) => {
-  const joke = req.body;
+router.post('/', (req, res, next) => {
+  const newJoke = req.body;
   newJoke.id = nanoid();
   jokes.push(newJoke);
-  res.json(newJoke);
+  res.json({
+    id: newJoke.id,
+  });
 });
 
 /**
@@ -64,13 +66,27 @@ router.get("/", (req, res, next) => {
  * Create a PATCH /joke/:id route, that updates the joke for the given id.
  */
 
-// …
+router.patch('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const jokeIndex = jokes.findIndex(joke => joke.id === id);
+  jokes[jokeIndex] = {
+    ...jokes[jokeIndex],
+    ...req.body,
+    id,
+  };
+});
 
 /**
  * Exercise 5
  * Create a DELETE /joke/:id route, that deletes the joke for the given id.
  */
 
-// …
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+  jokes = jokes.filter(joke => joke.id !== id);
+  res.status(200).json({
+    deletedId: id,
+  });
+});
 
 export default router;
